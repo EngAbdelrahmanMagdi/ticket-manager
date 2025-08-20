2ac382508dfd771013674139d332c27a78353c2d
EngAbdelrahmanMagdi <eng.abdelrahman.magdi@gmail.com>
2025-08-20 22:09:26 +0300

    Creating default tasks hardcoded array of data

---
+++ backend/config/tasks.php
@@ -0,0 +1,54 @@
+<?php
+
+return [
+    'default_tasks' => [
+        [
+            'id'=> 1,
+            'title'=> 'Make user login system',
+            'description'=> 'Build login and signup pages for users',
+            'status'=> 'Done'
+        ],
+        [
+            'id'=> 2,
+            'title'=> 'Design database tables',
+            'description'=> 'Create tables for users and their tasks',
+            'status'=> 'In Progress'
+        ],
+        [
+            'id'=> 3,
+            'title'=> 'Add API protection',
+            'description'=> 'Stop people from making too many requests',
+            'status'=> 'Pending'
+        ],
+        [
+            'id'=> 4,
+            'title'=> 'Send email alerts',
+            'description'=> 'Tell users when their tasks change',
+            'status'=> 'Pending'
+        ],
+        [
+            'id'=> 5,
+            'title'=> 'Make it work on phones',
+            'description'=> 'Check that the website looks good on mobile',
+            'status'=> 'In Progress'
+        ],
+        [
+            'id'=> 6,
+            'title'=> 'Make it faster',
+            'description'=> 'Use caching to load pages quicker',
+            'status'=> 'Pending'
+        ],
+        [
+            'id'=> 7,
+            'title'=> 'Write tests',
+            'description'=> 'Create tests to check everything works',
+            'status'=> 'Pending'
+        ],
+        [
+            'id'=> 8,
+            'title'=> 'Set up automatic deployment',
+            'description'=> 'Make the app deploy itself when we push code',
+            'status'=> 'Pending'
+        ]
+    ]
+];
